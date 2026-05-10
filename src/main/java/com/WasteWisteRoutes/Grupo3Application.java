7bef592d4451452f787d990b2a0c1fc9d65c67ab
Renzo C. <138859274+renzo-jcb@users.noreply.github.com>
2026-05-10 09:34:28 -0500

    Update Grupo3Application.java

--- src/main/java/com/WasteWisteRoutes/Grupo3Application.java
+++ src/main/java/com/WasteWisteRoutes/Grupo3Application.java
@@ -1,4 +1,4 @@
-package com.WasteWisteRoutes; // El paquete debe ser este
+package com.WasteWisteRoutes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Grupo3Application {
 
     public static void main(String[] args) {
-        // Aquí estaba el error, ya lo corregimos quitando "utils."
         SpringApplication.run(Grupo3Application.class, args);
     }
 
