6b89a27161f6ae0e07b86a4edf402fe9dd62853c
Gian Marco Chávez <gianmarcochavez2@gmail.com>
2026-05-10 22:55:19 -0500

    add reportes endpoint home

--- src/main/java/com/WasteWisteRoutes/controllers/ReporteController.java
+++ src/main/java/com/WasteWisteRoutes/controllers/ReporteController.java
@@ -1,11 +1,11 @@
 package com.WasteWisteRoutes.controllers;
+
 import com.WasteWisteRoutes.Entitys.Reporte;
-import com.WasteWisteRoutes.Entitys.Ubicacion;
-import com.WasteWisteRoutes.Repositories.UbicacionRepository;
 import com.WasteWisteRoutes.services.ReporteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -15,16 +15,30 @@ public class ReporteController {
     @Autowired
     private ReporteService reporteService;
 
+    @GetMapping
+    public String homeReportes() {
+        return "Endpoint de reportes funcionando";
+    }
+
     @PostMapping("/registrar")
     public ResponseEntity<Reporte> crearReporte(
             @RequestBody Reporte reporte,
             @RequestParam(required = false) String urlFoto) {
 
-        Reporte nuevoReporte = reporteService.crearReporteCompleto(reporte, reporte.getUbicacion(), urlFoto);
+        Reporte nuevoReporte = reporteService.crearReporteCompleto(
+                reporte,
+                reporte.getUbicacion(),
+                urlFoto
+        );
+
         return ResponseEntity.ok(nuevoReporte);
     }
+
     @GetMapping("/empresa/{empresaId}/pendientes")
     public List<Reporte> listarPendientes(@PathVariable Long empresaId) {
-        return reporteService.obtenerReportesPorEmpresaYEstado(empresaId, "Pendiente");
+        return reporteService.obtenerReportesPorEmpresaYEstado(
+                empresaId,
+                "Pendiente"
+        );
     }
 }
